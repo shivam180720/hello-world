ffcbeb667056ad0df3d6b88223a776fbe4c0be08
shivam180720 <9922103042@mail.jiit.ac.in>
2024-07-24 15:31:08 +0530

    Add files via upload

---
+++ add2numbers.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Enter any two numbers :";
+    int a,b;
+    cin>>a;
+    cin>>b;
+    cout<<"The sum of these 2 numbers are :";
+    cout<<a+b;
+}
